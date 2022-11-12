d8d602595086256538f675972e6eaa93a7432d7c
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-11-12 21:11:55 +0700

    update header name

--- resources/views/koleksi/daftarKoleksi.blade.php
+++ resources/views/koleksi/daftarKoleksi.blade.php
@@ -20,7 +20,8 @@
                 collumns: [{
                         "defaultContent": "-",
                         "targets": "_all"
-                    }, {
+                    },
+                    {
                         data: 'id',
                         name: 'id'
                     },
@@ -42,20 +43,22 @@
                         orderable: false,
                         searchable: false
                     },
-
                 ]
             });
         });
     </script>
     {{-- @endsection --}}
 
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Daftar Koleksi') }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    DAFTAR KOLEKSI
-
-                    <div class="row form-inline"></div>
                     <table class=" table table-striped table-hover" id="datatable">
                         <thead>
                             <tr>
